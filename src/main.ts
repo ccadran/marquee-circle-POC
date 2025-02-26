593e05e0a60062c34c1048276be7354ec987e260
ccadran <clar.cadran@gmail.com>
2025-02-26 15:51:59 +0100

    feat(archi): clean archi

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,41 @@
 import * as THREE from "three";
-import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
-import { FontLoader } from "three/addons/loaders/FontLoader.js";
-import { TTFLoader } from "three/examples/jsm/loaders/TTFLoader.js";
+import { MSDFTextGeometry, uniforms } from "three-msdf-text-utils";
+import vertexShader from "./shaders/vertex.glsl";
+import fragmentShader from "./shaders/fragment.glsl";
+import fnt from "./fonts/Syne-ExtraBold-msdf.json";
+import atlasURL from "./fonts/Syne-ExtraBold.png";
 
-// Types pour les shaders
-interface ShaderMaterialUniforms {
-  uTime: { value: number };
-  uRadius: { value: number };
-  uOvalFactor: { value: number };
-  [uniform: string]: THREE.IUniform<any>;
+// Définition des types
+interface Config {
+  textScaleMultiplier: number;
+  imgScaleMultiplier: number;
 }
 
-// Initialisation de la scène, caméra et rendu
-const canvas1 = document.querySelector("canvas.webgl1") as HTMLCanvasElement;
-const canvas2 = document.querySelector("canvas.webgl2") as HTMLCanvasElement;
-const container = document.querySelector(".container") as HTMLDivElement;
-const sizes = {
-  width: container.clientWidth,
-  height: container.clientHeight,
+// Configuration globale
+const config: Config = {
+  textScaleMultiplier: window.innerWidth < 440 ? 0.035 : 0.025,
+  imgScaleMultiplier: 0.003,
 };
-console.log(sizes);
 
-const scene1 = new THREE.Scene();
-const scene2 = new THREE.Scene();
+// INIT SCENE / CAMERA / RENDERER
+const initScene = (container: HTMLElement) => {
+  const canvas = document.querySelector("canvas.webgl1") as HTMLCanvasElement;
+  if (!canvas) throw new Error("Canvas not found");
 
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  1000
-);
-camera.position.z = 5;
-camera.position.y = -0.5;
+  const sizes = {
+    width: container.clientWidth,
+    height: container.clientHeight,
+  };
 
-const createRenderer = (canvas: HTMLCanvasElement): THREE.WebGLRenderer => {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    1000
+  );
+  camera.position.z = 5;
+
   const renderer = new THREE.WebGLRenderer({
     canvas,
     alpha: true,
@@ -41,150 +43,191 @@ const createRenderer = (canvas: HTMLCanvasElement): THREE.WebGLRenderer => {
   });
   renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-  return renderer;
+
+  return { scene, camera, renderer, sizes };
 };
 
-const renderer1 = createRenderer(canvas1);
-const renderer2 = createRenderer(canvas2);
-
-// Chargement de la police et création du texte
-const fontLoader = new FontLoader();
-let textMesh1: THREE.Mesh | undefined;
-let textMesh2: THREE.Mesh | undefined;
-let material1: THREE.ShaderMaterial;
-let material2: THREE.ShaderMaterial;
-
-const createTextMaterials = (
-  radius: number
-): [THREE.ShaderMaterial, THREE.ShaderMaterial] => {
-  const createMaterial = (isFront: boolean): THREE.ShaderMaterial =>
-    new THREE.ShaderMaterial({
-      vertexShader: `
-        uniform float uTime;
-        uniform float uRadius;
-        uniform float uOvalFactor;
-        varying vec3 vPosition;
-        void main() {
-          float angle = (position.x / uRadius) - uTime * 0.3;
-          vec3 newPosition = vec3(
-            uRadius * sin(angle),
-            position.y,
-            uRadius * cos(angle) * uOvalFactor
-          );
-          vPosition = newPosition;
-          gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
-        }
-      `,
-      fragmentShader: `
-        varying vec3 vPosition;
-        void main() {
-          if (${isFront ? "vPosition.z < 0.0" : "vPosition.z > 0.0"}) {
-            discard;
-          } else {
-            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-          }
-        }
-      `,
-      uniforms: {
-        uTime: { value: 0.0 },
-        uRadius: { value: radius },
-        uOvalFactor: { value: 0.5 },
-      } as ShaderMaterialUniforms,
-      transparent: true,
+// LOAD PLANE TEXTURE
+const loadTexture = (
+  src: string,
+  scene: THREE.Scene,
+  sizes: { width: number; height: number }
+): Promise<THREE.Mesh> => {
+  const textureLoader = new THREE.TextureLoader();
+  return new Promise((resolve) => {
+    textureLoader.load(src, (texture) => {
+      texture.colorSpace = THREE.SRGBColorSpace;
+      const planeGeometry = new THREE.PlaneGeometry(2, 2);
+      const planeMaterial = new THREE.MeshBasicMaterial({ map: texture });
+      const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+      const scale = sizes.width * config.imgScaleMultiplier;
+      plane.scale.set(scale, scale, scale);
+      scene.add(plane);
+      resolve(plane);
     });
-
-  return [createMaterial(true), createMaterial(false)];
+  });
 };
 
-const ttfLoader = new TTFLoader();
-
-ttfLoader.load("/fonts/Syne-ExtraBold.ttf", (fontData) => {
-  const font = fontLoader.parse(fontData);
-  const geometry = new TextGeometry(
-    "LE ZINE QUI RÉVEILLE VOTRE ÂME DE PIRATE LE ZINE QUI RÉVEILLE VOTRE ÂME DE PIRATE",
-    {
-      font,
-      size: 0.5,
-      depth: 0.1,
-    }
-  );
-
-  geometry.computeBoundingBox();
-  if (!geometry.boundingBox) return;
-
-  const targetWidth = sizes.width * 0.03;
-
-  let textWidth = geometry.boundingBox.max.x - geometry.boundingBox.min.x + 0.5;
-  const scale = targetWidth / textWidth;
-  geometry.scale(scale, scale, scale);
-
-  textWidth *= scale;
-
-  const radius = textWidth / (2 * Math.PI);
-
-  [material1, material2] = createTextMaterials(radius);
-
-  textMesh1 = new THREE.Mesh(geometry, material1);
-  textMesh2 = new THREE.Mesh(geometry.clone(), material2);
-
-  [textMesh1, textMesh2].forEach((mesh) => {
-    mesh.geometry.center();
-    mesh.rotation.set(0.25, 0.25, 0.25);
-    mesh.position.x = 0.05;
-    // mesh.position.y = 0.5;
+// LOAD FONT ATLAS
+const loadFontAtlas = (path: string): Promise<THREE.Texture> => {
+  return new Promise((resolve) => {
+    const loader = new THREE.TextureLoader();
+    loader.load(path, resolve);
   });
+};
 
-  scene1.add(textMesh1);
-  scene2.add(textMesh2);
-});
+// CALCULATE SCALE
+const calculateScale = (
+  textMesh: THREE.Mesh,
+  sizes: { width: number; height: number }
+) => {
+  textMesh.geometry.computeBoundingBox();
 
-const updateTextScale = () => {
-  if (!textMesh1 || !textMesh2) return;
-
-  const targetWidth = sizes.width * 0.03;
+  if (!textMesh.geometry.boundingBox) return { scale: 1, textWidth: 1 };
 
   const textWidth =
-    textMesh1.geometry.boundingBox!.max.x -
-    textMesh1.geometry.boundingBox!.min.x +
-    0.5;
-  const scale = targetWidth / textWidth;
+    textMesh.geometry.boundingBox.max.x -
+    textMesh.geometry.boundingBox.min.x +
+    20.5;
 
-  console.log("targetWidth", targetWidth);
-  console.log("textWidth", textWidth);
-  console.log("scale", scale);
+  const targetWidth = sizes.width * config.textScaleMultiplier;
 
-  textMesh1.scale.set(scale, scale, scale);
-  textMesh2.scale.set(scale, scale, scale);
+  return {
+    scale: targetWidth / textWidth,
+    textWidth,
+  };
 };
 
-// Animation
-const clock = new THREE.Clock();
-const tick = (): void => {
-  const elapsedTime = clock.getElapsedTime();
-  if (textMesh1 && textMesh2) {
-    material1.uniforms.uTime.value = elapsedTime;
-    material2.uniforms.uTime.value = elapsedTime;
-  }
+// MSDF TEXT MESH
+const createTextMesh = (
+  font: any,
+  atlas: THREE.Texture,
+  sizes: { width: number; height: number },
+  textContent: string
+) => {
+  const geometry = new MSDFTextGeometry({
+    text: textContent,
+    font: font,
+  });
 
-  renderer1.render(scene1, camera);
-  renderer2.render(scene2, camera);
-  requestAnimationFrame(tick);
+  const textMaterial = new THREE.ShaderMaterial({
+    side: THREE.DoubleSide,
+    transparent: true,
+    blending: THREE.AdditiveBlending,
+    defines: { IS_SMALL: false },
+    uniforms: {
+      ...uniforms.common,
+      ...uniforms.rendering,
+      ...uniforms.strokes,
+      uTime: { value: 0.0 },
+      uRadius: { value: 0 },
+      uOvalFactor: { value: 1.0 },
+      uMap: { value: atlas },
+    },
+    vertexShader,
+    fragmentShader,
+  });
+
+  const textMesh = new THREE.Mesh(geometry, textMaterial);
+
+  const { scale, textWidth } = calculateScale(textMesh, sizes);
+  textMesh.scale.set(scale, -scale, scale);
+
+  const radius = textWidth / (2 * Math.PI);
+  textMaterial.uniforms.uRadius.value = radius;
+
+  textMesh.rotation.x = 0.15;
+  textMesh.rotation.z = 0.15;
+
+  return { textMesh, textMaterial };
 };
-tick();
 
-const onWindowResize = (): void => {
-  sizes.width = container.clientWidth;
-  sizes.height = container.clientHeight;
+// UPDATE SCALE OF TEXT AND PLANE
+const updateTextScale = (
+  textMesh: THREE.Mesh,
+  plane: THREE.Mesh,
+  sizes: { width: number; height: number }
+) => {
+  if (!textMesh || !plane) return;
 
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
+  const planeScale = sizes.width * config.imgScaleMultiplier;
+  const { scale } = calculateScale(textMesh, sizes);
 
-  renderer1.setSize(sizes.width, sizes.height);
-  renderer2.setSize(sizes.width, sizes.height);
-
-  updateTextScale();
+  textMesh.scale.set(scale, -scale, scale);
+  plane.scale.set(planeScale, planeScale, planeScale);
 };
 
-window.addEventListener("resize", onWindowResize);
+// TICK ANIM
+const startAnimation = (
+  renderer: THREE.WebGLRenderer,
+  scene: THREE.Scene,
+  camera: THREE.Camera,
+  textMaterial: THREE.ShaderMaterial
+) => {
+  const clock = new THREE.Clock();
 
-onWindowResize();
+  const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+    textMaterial.uniforms.uTime.value = elapsedTime;
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(tick);
+  };
+  tick();
+};
+
+// RESIZE LISTENER
+const setupResizeListener = (
+  camera: THREE.PerspectiveCamera,
+  renderer: THREE.WebGLRenderer,
+  sizes: { width: number; height: number },
+  textMesh: THREE.Mesh,
+  plane: THREE.Mesh,
+  container: HTMLElement
+) => {
+  const onWindowResize = () => {
+    sizes.width = container.clientWidth;
+    sizes.height = container.clientHeight;
+
+    config.textScaleMultiplier = sizes.width < 440 ? 0.035 : 0.025;
+
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(sizes.width, sizes.height);
+    updateTextScale(textMesh, plane, sizes);
+  };
+
+  window.addEventListener("resize", onWindowResize);
+};
+
+// INIT ANIM
+const initMarquee = async () => {
+  const container = document.querySelector(".container") as HTMLElement;
+  if (!container) throw new Error("Container not found");
+
+  const { scene, camera, renderer, sizes } = initScene(container);
+
+  const imgElement = document.querySelector("img") as HTMLImageElement;
+  if (!imgElement) throw new Error("Image not found");
+
+  const plane = await loadTexture(imgElement.src, scene, sizes);
+  const atlas = await loadFontAtlas(atlasURL);
+
+  const textElement = document.querySelector(".text") as HTMLElement;
+  if (!textElement) throw new Error("Text element not found");
+
+  const textContent = textElement.textContent?.toUpperCase() || "DEFAULT TEXT";
+
+  const { textMesh, textMaterial } = createTextMesh(
+    fnt,
+    atlas,
+    sizes,
+    textContent
+  );
+  scene.add(textMesh);
+
+  startAnimation(renderer, scene, camera, textMaterial);
+  setupResizeListener(camera, renderer, sizes, textMesh, plane, container);
+};
+
+initMarquee();
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,3 @@
 /// <reference types="vite/client" />
+declare module "three-msdf-text-utils";
+declare module "*.glsl";
